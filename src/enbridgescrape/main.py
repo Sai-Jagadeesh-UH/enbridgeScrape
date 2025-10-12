dcb5c0a98119400b7c5466c71e3915e8732eaeaa
SaiJagadeeshY <ysaijagadeeshuh@gmail.com>
2025-10-12 14:31:41 -0500

    code modularized

--- src/enbridgescrape/main.py
+++ src/enbridgescrape/main.py
@@ -3,15 +3,15 @@ from playwright.async_api import async_playwright, Playwright
 import time
 from datetime import datetime, timedelta
 
-async def run(playwright: Playwright):
+async def run(playwright: Playwright, pipeCode: str):
     chromium = playwright.chromium 
 
-    browser = await chromium.launch(headless=True)
+    browser = await chromium.launch(headless=False)
     # context = await browser.new_context(accept_downloads=True) 
     page = await browser.new_page()
 
 
-    await page.goto(r"https://infopost.enbridge.com/InfoPost/AGHome.asp?Pipe=AG")
+    await page.goto(rf"https://infopost.enbridge.com/InfoPost/{pipeCode}Home.asp?Pipe={pipeCode}")
 
     await page.locator('li#Capacity.dropdown.sidebar-menu-item').click()
 
@@ -30,21 +30,16 @@ async def run(playwright: Playwright):
 
         await page.keyboard.press('Enter')
 
+
+    await iframe_locator.locator('a#lmapMaritimes.link').click()
+
     async with page.expect_download() as download_info:
-        await iframe_locator.locator("a#LinkButton1.link").get_by_text('Downloadable Format').click()
+        await iframe_locator.locator("a#LinkButton1.link").get_by_text('Download Csv').click()
 
-
-
-
-    # download = await page.wait_for_event("download")
     download = await download_info.value
 
-    # print(download.page)
-    # print(download.url)
-
-
-
-    await download.save_as("./" + download.suggested_filename)
+    # time.sleep(3)
+    await download.save_as("./downloads/OC/" + download.suggested_filename)
     
 
     # await context.close()
@@ -52,7 +47,7 @@ async def run(playwright: Playwright):
 
 async def main():
     async with async_playwright() as playwright:
-        await run(playwright)
+        await run(playwright, 'MNUS')
 
 
 if __name__=='__main__':
